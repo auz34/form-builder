8afaa13fda745530ff242776886794378eeff074
auz34 <andrewzavgorodniy@gmail.com>
2016-02-29 22:42:06 -0800

    blank property grid component

---
+++ app/components/PropertyGrid.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PropertyGrid extends Component {
+    render() {
+        return (
+            <div>
+                <h1>I am property grid</h1>
+            </div>
+        );
+    }
+}
+
+export default PropertyGrid;
